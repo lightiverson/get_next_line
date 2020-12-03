6a408ccd57f756585b21d6f67cba400fbe5a7044
Kawish Gajadien <kawishg@gmail.com>
2020-12-03 18:03:45 +0100

    Puts next line out to terminal.

--- main.c
+++ main.c
@@ -19,33 +19,51 @@ int main(void)
 	int fd;
 	char buf[BUFFER_SIZE]; // buf is elke keer BUFFER_SIZE aantal characters van fd.
 	int n;
-	char *s;
-
+	char *ans;
+	char *temp;
+	int flag;
+	
 	fd = open("tester.txt", O_RDONLY, 0);
-
+	flag = 0;
 	while ((n = read(fd, buf, BUFFER_SIZE)) > 0)
 	{
+		// ft_putstr_fd(buf, 1);
+		// write(1, "\n", 1);
+		if (flag)
+		{
+			ft_putstr_fd("ft_putstr_fd() = ", 1);
+			ft_putstr_fd(ans, 1);
+			write(1, "\n", 1);
+			// ft_putstr_fd("temp vol", 1);
+			// write(1, "\n", 1);
+			ans = bzero(ans, strlen(ans));
+			ans = ft_strjoin(ans, temp);
+			temp = bzero(temp, strlen(temp));
+			flag = 0;
+		}
+
 		if (strchr(buf, '\n'))
 		{
-			char *c = strchr(buf, '\n');
-			int index = (int)(c - buf);
+			int index_newline = (int)(strchr(buf, '\n') - buf);
+			// printf("\nindex_newline = %d\n", index_newline);
 
-			char *last_block_before_nl = ft_substr(buf, 0, index + 1);
-			s = ft_strjoin(s, last_block_before_nl);
-			ft_putstr_fd(s, 1);
-			return(0);
+			char *alles_voor_newline = ft_substr(buf, 0, index_newline);
+			// ft_putstr_fd(alles_voor_newline, 1); // = 3?
+			// write(1, "\n", 1);
+
+			ans = ft_strjoin(ans, alles_voor_newline);
+
+			temp = ft_substr(buf, (index_newline + 1), strlen(buf));
+			// ft_putstr_fd(temp, 1); // = di, = Di
+			// write(1, "\n", 1);
+			flag = 1;
 		}
-		
-		// als er een newline is gevonden
-		// dan moet je s concaten met buf[tot newline]
-		// dus er moet een nieuwe array zijn vanaf buf[0] tot en met buf[index newline]
-			// hoe krijg je de index terug van char in string? Door strchr - buf te doen, en int van te maken.
-			// hoe krijg je een substring terug van index tot index? ft_substr(buf, 0, index + 1).
-			// hoe kan ik dit testen? bij die return in de if statement moet s 1 regel zijn.
-
-		// ft_putstr_fd(buf, 1);
-		s = ft_strjoin(s, buf);
+		else
+		{
+			ans = ft_strjoin(ans, buf);
+		}
 	}
-	ft_putstr_fd(s, 1);
+	ft_putstr_fd("ft_putstr_fd() = ", 1);
+	ft_putstr_fd(ans, 1);
 	return (0);
 }
